8aaea5a6413a142e60e314aef17880758982d730
KizMan-23 <kizitougwu777@gmail.com>
2026-03-08 20:07:55 +0100

    access changes to the models and documents

--- first_rag/default_rag.py
+++ first_rag/default_rag.py
@@ -1,15 +1,19 @@
+import os
 from pathlib import Path
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
 from llama_index.core import (
     VectorStoreIndex,
     SimpleDirectoryReader,
     StorageContext,
     load_index_from_storage,
+    Settings,
 )
 
-BASE_DIR = Path(__file__).resolve().parent()
+Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+BASE_DIR = Path(__file__).resolve().parent
 PERSIST_DIR = BASE_DIR / "storage"
-DATA_DIR = BASE_DIR /"documents" / "HHRG-118-JU00-20240312-SD001.pdf"
+DATA_DIR = r"C:\Users\Hp Pc\Documents\HHRG-118-JU00-20240312-SD001.pdf"
 
 def get_index(persist_dir=PERSIST_DIR, data_file=DATA_DIR):
     if persist_dir.exists():
@@ -35,7 +39,7 @@ def main():
     print(response)
 
 
-if __name__ =="__main___":
+if __name__ == "__main__":
     main()
 
 
