d71171394665295700ad63d27a9514b475df4313
KizMan-23 <kizitougwu777@gmail.com>
2026-04-22 20:11:30 +0100

    working with excel files for MongoDB Atlas

--- vector_search/answer_app/load_data.py
+++ vector_search/answer_app/load_data.py
@@ -1,24 +1,36 @@
 from pymongo import MongoClient
 from langchain_ollama import OllamaEmbeddings
 from langchain_mongodb import MongoDBAtlasVectorSearch
-from langchain_community.document_loaders import DirectoryLoader
-from langchain.llms import Qwen3
-from langchain.chains import RetrievalQA
+from langchain_community.document_loaders import DirectoryLoader, UnstructuredExcelLoader
+# from langchain.llms import Qwen3
+# from langchain.chains import RetrievalQA
 import os
+import glob
 from dotenv import load_dotenv
 
 load_dotenv()
 
-client = os.getenv("MONGO_URL")
+client = MongoClient(os.getenv("MONGO_URI"))
+
 dbName = "langchain_demo"
-collectionName = "collection_of_cv_blobs"
+collectionName = "collection_of_excel_blobs"
 collection = client[dbName][collectionName]
 
-dir_path = r"C:\Users\Hp Pc\Documents\CVs"
+dir_path = r"C:\Users\Hp Pc\Desktop\SEMI\Documents\Excel Docs\\"
 
-loader = DirectoryLoader(dir_path, glob="**/*.txt", show_progress=True)
+# print(f"Path exists: {os.path.exists(dir_path)}")
+# files = glob.glob(os.path.join(dir_path, "*.xlsx"), recursive=True)
+# print(f"Files found by glob: {len(files)}")
+
+loader = DirectoryLoader(
+    dir_path, 
+    glob="*.xlsx", 
+    loader_cls=UnstructuredExcelLoader, 
+    loader_kwargs={"mode": "elements"},
+    show_progress=True)
 data = loader.load()
 
-embedding = OllamaEmbeddings("Qwen/Qwen3-Embedding-8B")
 
-vector_store = MongoDBAtlasVectorSearch(data, embedding, collection=collection)
+embedding = OllamaEmbeddings(model="qwen3-embedding:8b", dimensions=1024)
+
+vector_store = MongoDBAtlasVectorSearch.from_documents(data, embedding, collection=collection)
